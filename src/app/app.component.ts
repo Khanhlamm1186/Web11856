e863c3eab4689dca9899a3e6af8d62677d8dac08
Khanhlam1186 <khanhyt2002@gmail.com>
2022-10-12 12:11:04 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Web1186';
+  value: string = "https://images.pexels.com/photos/2382325/pexels-photo-2382325.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+  index: number=1;
+  imgs:string[]=["https://images.pexels.com/photos/2382325/pexels-photo-2382325.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260","https://images.pexels.com/photos/3578393/pexels-photo-3578393.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260","https://images.pexels.com/photos/4484184/pexels-photo-4484184.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"];
+  changeImg(){
+      this.value=this.imgs[this.index];
+      this.index++;
+      if(this.index==3){
+        this.index=0;
+      }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SanphamComponent } from './sanpham/sanpham.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SanphamComponent
   ],
   imports: [
     BrowserModule,
